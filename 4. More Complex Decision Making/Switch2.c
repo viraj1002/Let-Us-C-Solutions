3b7ad62f787b9694573ba61426fb279af9bfa768
Viraj Gund <118049708+viraj1002@users.noreply.github.com>
2024-01-05 23:29:59 +0530

    Create Switch2.c

---
+++ 4. More Complex Decision Making/Switch2.c
@@ -0,0 +1,67 @@
+// C program to print the day using switch
+#include <stdio.h>
+ 
+// Driver Code
+
+int main()
+{
+
+    int day = 2;
+ 
+
+    printf("The day with number %d is ", day);
+
+    switch (day) {
+
+      case 1:
+
+          printf("Monday");
+
+          break;
+
+      case 2:
+
+          printf("Tuesday");
+
+          break;
+
+      case 3:
+
+          printf("Wednesday");
+
+          break;
+
+      case 4:
+
+          printf("Thursday");
+
+          break;
+
+      case 5:
+
+          printf("Friday");
+
+          break;
+
+      case 6:
+
+          printf("Saturday");
+
+          break;
+
+      case 7:
+
+          printf("Sunday");
+
+          break;
+
+      default:
+
+          printf("Invalid Input");
+
+          break;
+
+      }
+
+    return 0;
+}
